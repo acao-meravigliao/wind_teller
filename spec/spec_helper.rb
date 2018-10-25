3cb42607d54639e59fd2f866b2a853a7b72be66f
Daniele Orlandi <daniele@orlandi.com>
2018-10-26 00:58:15 +0200

    Added rspec and gitlab-ci deployment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
-RSpec.configure do |c|
+require 'bundler/setup'
+require 'ygg/exception'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
--- wind_teller.gemspec
+++ wind_teller.gemspec
@@ -25,8 +25,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ['lib']
 
-  # specify any dependencies here; for example:
-  # s.add_development_dependency 'rspec'
+  s.add_development_dependency 'rspec'
 
   s.add_runtime_dependency 'ygg_agent', '~> 2.8'
   s.add_runtime_dependency 'serialport', '~> 1.3'
